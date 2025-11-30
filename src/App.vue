cb073b7766fd1cc240b4851d0c6597e675b773ab
yato322 <maxwol2006@gmail.com>
2025-11-30 17:59:08 +0300

    инициализация репозитория

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { UiButton, UiInput, UiSelect, UiCardResult } from 'calc_anime'
+import { useIpCalculator } from './components/useIpCalculator'
+
+const {
+  ip,
+  mask,
+  options,
+  isShowResult,
+  isIpValid,
+  networkAddress,
+  addressesCount,
+  showResult,
+} = useIpCalculator()
+</script>
+
+<template>
+  <form class="form">
+ <UiInput v-model="ip" placeholder="Введите IP адрес" />
+
+  <UiSelect :options="options" v-model="mask" />
+
+  <UiButton variant="primary" :isDisabled="!isIpValid()" @click="showResult">Рассчитать</UiButton>
+  </form>
+<UiCardResult v-if="isShowResult && isIpValid()" :ip="ip" :mask="mask" :networkAddress="networkAddress" :addressesCount="addressesCount" />
+</template>
+
+<style>
+.form {
+  display: flex;
+  gap: 4px;
+}
+</style>
